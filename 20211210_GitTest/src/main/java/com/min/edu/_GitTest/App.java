41a00161b4392f06e02f59fedb346d04e308e1b6
boyoung <boyoung098@naver.com>
2021-12-10 13:42:14 +0900

    12/10

---
+++ 20211210_GitTest/src/main/java/com/min/edu/_GitTest/App.java
@@ -0,0 +1,13 @@
+package com.min.edu._GitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
